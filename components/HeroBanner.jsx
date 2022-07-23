9975c19b71d7b4428f0602e403b8ba8f38728e2d
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-22 22:22:48 -0700

    Dynamically fetching product data
    We are now fetching product data from our Sanity API.

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
-function HeroBanner() {
+
+import { urlFor } from '../lib/client'
+
+function HeroBanner({heroBanner}) {
   return (
     <div className="hero-banner-container">
         <div>
-            <p className='beats-solo'> SMALL TEXT</p>
-            <h3>MID TEXT</h3>
-            <img src="" alt="headphones"
+            <p className='beats-solo'> {heroBanner.smallText}</p>
+            <h3>{heroBanner.midText}</h3>
+            <h1>{heroBanner.largeText1 }</h1>
+            <img src={urlFor(heroBanner.image)} alt="headphones"
             className="hero-banner-image" /> 
             <div>
                 <Link href="/product/ID">
                     <button type="button">
-                        BUTTON TEXT
+                        {heroBanner.buttonText  }
                     </button>
                 </Link>
                 <div className="desc">
                     <h5>Description</h5>
-                    <p> DESCRIPTION</p>
+                    <p> {heroBanner.desc}</p>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 import { client } from '../lib/client';
   
 
-const Home = () => {
+const Home = ({ products, bannerData}) => {
   return (
 <div>
-    <HeroBanner />
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
     <div className="products-heading">
       <h2>Best Selling Products</h2>
       <p>Speakers of many variations</p>
     </div>
 
     <div className="products-container">
-      {['Product 1', 'Product 2'].map((product)=>product)}
+      {products?.map((product)=>product.name)}
     </div>
     
     <FooterBanner />
