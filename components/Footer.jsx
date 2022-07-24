bf12175a790d378eca60136292ade2caa1973187
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-23 20:18:21 -0700

    Layout : { footer, navbar}
    created layout page that houses our navbar, content, and footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+<div className="footer-container">
+  <p>2022 JSM Headphones All rights reserved</p>
+  <p className="icons">
+    <AiFillInstagram />
+    <AiOutlineTwitter />
+  </p>
+</div>  )
 }
 
 export default Footer
