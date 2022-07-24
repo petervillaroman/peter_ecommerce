803864d879c37af5ef853d4b041a74722e3aedc3
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-23 19:36:56 -0700

    dynamically display Products
    In index.js we are now mapping through our array of products from sanity and creating a product component for each item.
    
    In product.jsx we created the structure of the product component and then styled it using the classes preset in styles.css.

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+/** @format */
 
-function Product() {
-  return (
-    <div>Product</div>
-  )
-}
+import React from 'react';
+import { urlFor } from '../lib/client';
+import Link from 'next/link';
 
-export default Product
+ 
+const Product = ({ product: { image, name, slug, price } }) => {
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`}>
+				<div className='product-card'>
+					<img
+						src={urlFor(image && image[0])}
+						width={250}
+						height={250}
+						className='product-image'
+					/>
+          <p className="product-name">{name}</p>
+          <p className="product-name">${price}</p>
+				</div>
+			</Link>
+		</div>
+	);
+};
+
+export default Product;
+ 
