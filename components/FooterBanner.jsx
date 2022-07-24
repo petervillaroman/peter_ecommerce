563ce69d3b47d74be1124ca7b279be133d91ee23
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-07-23 21:35:21 -0700

    [slug] reformat // Footerbanner fix
    fixed footerbanner bug and reformatted [slug].js

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -15,13 +15,14 @@ const FooterBanner = ({ footerBanner:{
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
-				<div className='right'></div>
+				<div className='right'>
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
+          </div>
 			</div>
 
       <img src={urlFor(image)} className="footer-banner-image" />
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,19 +13,30 @@ import Product from '../../components/Product';
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product;
 
-    const [index, setIndex] = useState(0);
+	const [index, setIndex] = useState(0);
 	return (
 		<div>
 			<div className='product-detail-container'>
 				<div>
 					<div className='image-container'>
-						<img src={urlFor(image && image[index])} className="product-detail-image" />   
+						<img
+							src={urlFor(image && image[index])}
+							className='product-detail-image'
+						/>
 					</div>
-					<div className="small-images-container">
-                        {image?.map((item, i) => (
-                            <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} />
-                        ))}
-                    </div>
+					<div className='small-images-container'>
+						{image?.map((item, i) => (
+							<img
+								src={urlFor(item)}
+								className={
+									i === index
+										? 'small-image selected-image'
+										: 'small-image'
+								}
+								onMouseEnter={() => setIndex(i)}
+							/>
+						))}
+					</div>
 				</div>
 				<div className='product-detail-desc'>
 					<h1>{name}</h1>
@@ -56,22 +67,29 @@ const ProductDetails = ({ product, products }) => {
 							</span>
 						</p>
 					</div>
-                    <div className="buttons">
-                        <button type="button" className='add-to-cart' onClick="">Add to Cart</button>
-                        <button type="button" className='buy-now' onClick="">Buy Now</button>
-                    </div>
+					<div className='buttons'>
+						<button
+							type='button'
+							className='add-to-cart'
+							onClick=''>
+							Add to Cart
+						</button>
+						<button type='button' className='buy-now' onClick=''>
+							Buy Now
+						</button>
+					</div>
 				</div>
 			</div>
-            <div className="maylike-products-wrapper">
-                <h2>You may also like</h2>
-                <div className="marquee">
-                    <div className="maylike-products-container track">
-                        {products.map((item) => (
-                            <Product key={item._id} product={item}/>
-                        ))}
-                    </div>
-                </div>
-            </div>
+			<div className='maylike-products-wrapper'>
+				<h2>You may also like</h2>
+				<div className='marquee'>
+					<div className='maylike-products-container track'>
+						{products.map((item) => (
+							<Product key={item._id} product={item} />
+						))}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
