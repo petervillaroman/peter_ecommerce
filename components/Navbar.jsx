5eb94a57a8332ba79216487271c2f8305063d3a9
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-05-30 23:06:02 -0700

    pushing last known edits.
    It's been a year since I last wrote on this. This was my last pending commit so Ill just push it.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 	return (
 		<div className='navbar-container'>
 			<p className='logo'>
-				<Link href='/'> JSM Headphones</Link>
+				<Link href='/'> KP's Emporium</Link>
 			</p>
 
 			<button
